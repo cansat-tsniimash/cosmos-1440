751f84c9e7fe46323b100c5694e8399a57616aab
notKorolev <boroninzena7@gmail.com>
2026-03-03 17:38:34 +0300

    вспомогательная библиотека для bmp280

---
+++ src/cosmos1440/app/delay/i2c-crutch.h
@@ -0,0 +1,16 @@
+/*
+ * i2c-crutch.h
+ *
+ *  Created on: 12 нояб. 2022 г.
+ *      Author: Agnus
+ */
+
+#include "stm32f4xx.h"
+
+#ifndef INC_I2C_CRUTCH_H_
+#define INC_I2C_CRUTCH_H_
+
+void I2C_ClearBusyFlagErratum(I2C_HandleTypeDef *hi2c, uint32_t timeout);
+void reset_i2c_1();
+
+#endif /* INC_I2C_CRUTCH_H_ */
