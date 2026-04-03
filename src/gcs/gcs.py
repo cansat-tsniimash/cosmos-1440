f7f87af5439efe471feb42753e5755551495248a
Gogol <kupapa052@gmail.com>
2026-04-03 17:52:49 +0300

    Доделали разбор пакета

--- src/gcs/gcs.py
+++ src/gcs/gcs.py
@@ -6,10 +6,12 @@ port = serial.Serial("COM8", baudrate=115200)
 port.timeout = 0.1
 p = magic.Parser()
 
-with open("file.bin", mode = "wb") as stream:
+with open("file.bin", mode = "wb") as stream, open("file.csv", mode="w") as csv:
     while True:
         data = port.read(4096)
         stream.write(data)
         packets = p.parse(data)
         for packet in packets:
-            print(packet)
+            numbers = magic.convert(packet)
+            csv.write(";".join([str(x).replace("." , ",") for x in numbers]) + "\n")
+            print(packet, numbers)
--- src/gcs/magic.py
+++ src/gcs/magic.py
@@ -23,7 +23,6 @@ class Parser:
             if cchk == 0:
                 walues = struct.unpack("<HIhI3h3hB", packet)
                 rv.append(walues)
-                print(walues)
                 data = data[27:]
             else:
                 data = data[1:]
@@ -31,3 +30,47 @@ class Parser:
 
         self.leftowers = data
         return rv
+    
+
+def convert(valeus: tuple):
+    return(
+    valeus[0],#time id
+    valeus[1],#time
+    valeus[2]/16, #temperature
+    valeus[3], #давление
+    lsm6ds3_from_fs16g_to_mg(valeus[4]),
+    lsm6ds3_from_fs16g_to_mg(valeus[5]),
+    lsm6ds3_from_fs16g_to_mg(valeus[6]),
+    lsm6ds3_from_fs2000dps_to_mdps(valeus[7]),
+    lsm6ds3_from_fs2000dps_to_mdps(valeus[8]),
+    lsm6ds3_from_fs2000dps_to_mdps(valeus[9]),
+)
+
+
+def lsm6ds3_from_fs2g_to_mg(lsb: int) -> float:
+  return lsb * 61.0 / 1000.0
+
+def lsm6ds3_from_fs4g_to_mg(lsb: int) -> float:
+  return lsb * 122 / 1000.0
+
+def lsm6ds3_from_fs8g_to_mg(lsb: int) -> float:
+  return lsb * 244.0 / 1000.0
+
+def lsm6ds3_from_fs16g_to_mg(lsb: int) -> float:
+  return lsb * 488.0 / 1000.0
+
+def lsm6ds3_from_fs125dps_to_mdps(lsb: int) -> float:
+  return lsb * 4375.0 / 1000.0
+
+def lsm6ds3_from_fs250dps_to_mdps(lsb: int) -> float:
+  return lsb * 8750.0 / 1000.0
+
+def lsm6ds3_from_fs500dps_to_mdps(lsb: int) -> float:
+  return lsb * 1750.0 / 1000.0
+
+def lsm6ds3_from_fs1000dps_to_mdps(lsb: int) -> float:
+  return lsb * 35.0
+
+def lsm6ds3_from_fs2000dps_to_mdps(lsb: int) -> float:
+  return lsb * 70.0
+
